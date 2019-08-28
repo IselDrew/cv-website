f56f7319d31247d921d7baadf1cbbcd25f0170b9
IselDrew <andryshonochek0604@gmail.com>
2019-08-28 16:06:22 +0300

    Add projects-page content

--- src/ResumeData.js
+++ src/ResumeData.js
@@ -68,3 +68,27 @@ export const teamList = [
       "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
   }
 ];
+
+export const projectsList = [
+  {
+    id: 11,
+    img: "https://avatars2.githubusercontent.com/u/9293098?s=88&v=4",
+    title: "title",
+    text:
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+  },
+  {
+    id: 12,
+    img: "https://avatars2.githubusercontent.com/u/9293098?s=88&v=4",
+    title: "title",
+    text:
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+  },
+  {
+    id: 13,
+    img: "https://avatars2.githubusercontent.com/u/9293098?s=88&v=4",
+    title: "title",
+    text:
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+  }
+];
