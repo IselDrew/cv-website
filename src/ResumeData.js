29883c81b7d73f4665c3c2437fe2d04b1c481f8e
IselDrew <andryshonochek0604@gmail.com>
2019-09-05 21:46:53 +0300

    Add paragraphs titles to home page

--- src/ResumeData.js
+++ src/ResumeData.js
@@ -1,48 +1,53 @@
-import uniqid from "uniqid";
+import uniqid from "uniqid"
 
 export const titles = [
   {
     name: "home",
     id: uniqid(),
-    url: "/"
+    url: "/",
   },
   {
     name: "about",
     id: uniqid(),
-    url: "/about"
+    url: "/about",
   },
   {
     name: "projects",
     id: uniqid(),
-    url: "/projects"
+    url: "/projects",
   },
   {
     name: "contact",
     id: uniqid(),
-    url: "/contact"
-  }
-];
+    url: "/contact",
+  },
+]
 
 export const paragraphs = [
   {
     id: uniqid(),
-    img: "https://pbs.twimg.com/profile_images/1075815486221299712/K8c4i-oC_400x400.jpg",
+    title: "title",
+    img:
+      "https://pbs.twimg.com/profile_images/1075815486221299712/K8c4i-oC_400x400.jpg",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
-    img: "http://icons.iconarchive.com/icons/th3-prophetman/game/256/Diablo-II-2-icon.png",
+    title: "title",
+    img:
+      "http://icons.iconarchive.com/icons/th3-prophetman/game/256/Diablo-II-2-icon.png",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
+    title: "title",
     img: "http://mgnews.ru/post/topimage563x346/43682/warcraft-3-logo.jpg",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
-  }
-];
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
+  },
+]
 
 export const teamList = [
   {
@@ -51,46 +56,51 @@ export const teamList = [
     name: "name",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
-    img: "https://pbs.twimg.com/profile_images/285647546/IMG_2024_edited_400x400.JPG",
+    img:
+      "https://pbs.twimg.com/profile_images/285647546/IMG_2024_edited_400x400.JPG",
     name: "name",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
-    img: "https://pbs.twimg.com/profile_images/1002021179186544641/_c9ugmrm_400x400.jpg",
+    img:
+      "https://pbs.twimg.com/profile_images/1002021179186544641/_c9ugmrm_400x400.jpg",
     name: "name",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
-  }
-];
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
+  },
+]
 
 export const projectsList = [
   {
     id: uniqid(),
-    img: "https://www.logolynx.com/images/logolynx/5f/5f7582061c4c0944f56e4fe3c7ff8710.jpeg",
+    img:
+      "https://www.logolynx.com/images/logolynx/5f/5f7582061c4c0944f56e4fe3c7ff8710.jpeg",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
-    img: "http://images1.wikia.nocookie.net/__cb20120208103143/wow/ru/images/9/9f/ScourgeCrest.jpg",
+    img:
+      "http://images1.wikia.nocookie.net/__cb20120208103143/wow/ru/images/9/9f/ScourgeCrest.jpg",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
   },
   {
     id: uniqid(),
-    img: "https://i.pinimg.com/originals/e4/58/75/e458759bac114406af8730159bca960b.jpg",
+    img:
+      "https://i.pinimg.com/originals/e4/58/75/e458759bac114406af8730159bca960b.jpg",
     title: "title",
     text:
-      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font."
-  }
-];
+      "I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
+  },
+]
